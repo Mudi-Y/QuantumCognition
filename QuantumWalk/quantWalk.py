a50f7d3172eb7e1d8c077b6906344014a8cd34b5
Mudi Yang <mudiyang1999@gmail.com>
2022-05-04 03:44:15 +0000

    improved plotting

--- QuantumWalk/quantWalk.py
+++ QuantumWalk/quantWalk.py
@@ -56,22 +56,22 @@ system_state = [Binary(f'qubit_{i}_state') for i in range(m)]
 
 #obj: [ 4 x_0*x_1 + 4 x_1*x_2 + 4 x_2*x_3 ]/2 -2
 cqm.set_objective((4*system_state[0]*system_state[1] + 4*system_state[1]*system_state[2] + 4*system_state[2]*system_state[3])/2 - 2)
-
 sum_to_one = cqm.add_constraint(sum(system_state) == 1, label='sum to one')
 
 sampler = LeapHybridCQMSampler()
 sampleset = sampler.sample_cqm(cqm, time_limit=10)
 
 feasible_sampleset = sampleset.filter(lambda row: row.is_feasible)
-results = [str(sampleset.record[i][0]) for i in range(len(feasible_sampleset.record))]
+num_feasible = len(feasible_sampleset.record)
+results = [str(sampleset.record[i][0]) for i in range(num_feasible)]
 results.sort()
-
 
 #plot histogram of results
 results_count = Counter(results)
 plt.bar(results_count.keys(), results_count.values())
 plt.xlabel("Final State")
 plt.ylabel("Count")
+plt.title(f"Final States after {num_feasible} Samples")
 plt.xticks(rotation=0)
 plt.savefig('/workspace/QuantumCognition/fig3')
 
