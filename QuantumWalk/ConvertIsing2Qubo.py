c1f5a79e24c4bc60b564bbaa5bdf72c7029b9b73
Mudi Yang <mudiyang1999@gmail.com>
2022-06-02 03:21:51 +0000

    fixed typo in linear

--- QuantumWalk/ConvertIsing2Qubo.py
+++ QuantumWalk/ConvertIsing2Qubo.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 m=4
 num_reads = 1000
 
-linear = {('q1','q1'):0.25, ('q1','q1'):0.5, ('q1','q1'):0.75, ('q1','q1'):1}
+linear = {('q1','q1'):0.25, ('q2','q2'):0.5, ('q3','q3'):0.75, ('q4','q4'):1}
 qudratic = {('q1','q2'):2.0/3.0, ('q2','q3'):2.0/3.0, ('q3','q4'):2.0/3.0}
 
 # qubo = dimod.ising_to_qubo(linear, qudratic) #converting ising to qubo
