7b12db4566eedd26ccfcb6f2c3e53342daef1dc7
Mudi Yang <mudiyang1999@gmail.com>
2023-07-03 17:18:19 +0000

    Ising Model annealing and encoded Ising annealing

--- QuantumWalk/IsingAnnealer/ising_annealer.py
+++ QuantumWalk/IsingAnnealer/ising_annealer.py
@@ -1,8 +1,11 @@
 # /usr/local/bin/python -i /workspace/QuantumCognition/QuantumWalk/IsingAnnealer/ising_annealer.py
 
-from ising_model import IsingModel
+from ising_encoded_model import IsingModel
 a = IsingModel(q=2, t=1, sigmaSq=2, delta=4)
 
-# res = a.annealQPU()
-res = a.calculateEnergy([1,-1,1,-1])
+res = a.annealQPU()
 print(res, end = "\n")
+a.eigs()
+
+
+# 1/2|00> + 1/4|01> + 1/2|10> + 1/4|11>
